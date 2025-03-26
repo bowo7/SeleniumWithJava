dc189be44ef2753d2e617c30c9c51824a9149777
bowo7 <bowomars@gmail.com>
2025-03-26 21:03:17 +0700

    New Update

--- src/main/java/org/example/Locators2.java
+++ src/main/java/org/example/Locators2.java
@@ -8,35 +8,42 @@ import org.testng.Assert;
 import java.time.Duration;
 
 public class Locators2 {
-    public static void main(String[] arg) {
+    public static void main(String[] arg) throws InterruptedException {
 
         String userName = "rahul";
-        String password = "rahulshettyacademy";
+//        String password = "rahulshettyacademy";
 
         //Login dan Logout
         WebDriver driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        String password = getPassword(driver);
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
         driver.findElement(By.id("inputUsername")).sendKeys(userName);
         driver.findElement(By.name("inputPassword")).sendKeys(password);
         driver.findElement(By.className("signInBtn")).click();
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        };
+        Thread.sleep(5000);
         System.out.println(driver.findElement(By.tagName("p")).getText());
 //        Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
 //        Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2"))
 //                .getText(),"Hello " +userName+",");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.findElement(By.className("logout-btn")).click();
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        };
+        Thread.sleep(5000);
         driver.close();
 
     }
+
+    public static String getPassword(WebDriver driver) throws InterruptedException {
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("button.reset-pwd-btn")).click();
+        String passwordText = driver.findElement(By.cssSelector("form p")).getText();
+        String[] passwordArray = passwordText.split("'");
+//        String[] passwordArray2 = passwordArray[1].split("'");
+//        passwordArray2[0]
+        String password = passwordArray[1].split("'")[0];
+        return password;
+    }
+
 }
