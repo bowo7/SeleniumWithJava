9e913cc3c23e7b7643736df3c6cfa475bb9d124d
bowo7 <bowomars@gmail.com>
2025-03-26 20:31:53 +0700

    Update

--- src/main/java/org/example/Locators2.java
+++ src/main/java/org/example/Locators2.java
@@ -13,17 +13,30 @@ public class Locators2 {
         String userName = "rahul";
         String password = "rahulshettyacademy";
 
+        //Login dan Logout
         WebDriver driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
         driver.findElement(By.id("inputUsername")).sendKeys(userName);
         driver.findElement(By.name("inputPassword")).sendKeys(password);
         driver.findElement(By.className("signInBtn")).click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        };
         System.out.println(driver.findElement(By.tagName("p")).getText());
-        Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
-        Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2"))
-                .getText(),"Hello " +userName+",");
+//        Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
+//        Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2"))
+//                .getText(),"Hello " +userName+",");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.findElement(By.className("logout-btn")).click();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        };
+        driver.close();
 
     }
 }
